9aa0f9d59d8edb65d31af4d38877ee4f64aac470
Gabriel Klafke Teixeira <143045986+gazinoigaoi@users.noreply.github.com>
2024-08-28 09:56:12 -0300

    inicial

--- index.php
+++ index.php
@@ -4,8 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Teste</h1>
+
+<div class="container">
+    <header>
+        <div class="logoDiv">
+
+           <img class="logo" src="image/logo.png" alt="">
+            <h1>BookHub</h1> 
+        </div>
+        
+        <div class="pesquisar"> 
+            <input class="inputPesquisar" type="text" >
+            <img class="loupe" src="image/loupe.png" alt="">
+        </div>
+
+        <div class="containerFiltro">
+
+            <button class="btnFiltro" type="button"><img class="imgFilter" src="image/filterLinesW.png" alt=""></button>
+
+        </div>
+    </header>
+    
+    <main>
+        
+        <div class="containerLivros">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        </div>
+
+    </main>
+    
+    
+
+    <footer>
+        informações do site
+    </footer>
+
+    
+    </div>
 </body>
 </html>
