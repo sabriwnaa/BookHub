dde4724c18c96e0ae286d72d5fb316230fb15219
GabrielKKT <164067823+GabrielKKT@users.noreply.github.com>
2024-09-04 18:54:50 -0300

    fetie

--- livro/addLivro.php
+++ livro/addLivro.php
@@ -4,7 +4,8 @@
         $db = new mysqli("localhost", "root", "", "bookhub");
     
 
-        $diretorioDestino = "../image/";
+        $diretorioDestino = $_SERVER['DOCUMENT_ROOT'] . "/BookHub/image/";
+
         if($_FILES['capa']['error'] != 0 ){
          header("location:../index.html");
         }
@@ -12,8 +13,11 @@
         //Caminho onde o imagem será armazenado
         $destinoCapa = $diretorioDestino.$_FILES['capa']['name'];
 
+        //adiciona a imagem no php aqui
+        move_uploaded_file($_FILES['capa']['tmp_name'], $destinoCapa);
+
         //Query de consulta
-        $query = "insert into livro (titulo, id, ano, capa) values ('{$_POST['titulo']}',{$_POST['autor']},{$_POST['ano']},'{$destinoCapa}')";
+        $query = "insert into livro (titulo, ano, idAutor, capa) values ('{$_POST['titulo']}',{$_POST['ano']},{$_POST['autor']},'{$destinoCapa}')";
 
         //Executa a consulta e armazena o resultado
         $resultado = $db->query($query);
